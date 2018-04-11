a5ab4e09a78f6d7c2b015f89935311e05b2002b3
Marta Aguilar <martaguilar.soto@gmail.com>
2018-04-11 14:20:21 +0200

    create funcional page, add task and class for opacity and hidden part

--- js/main.js
+++ js/main.js
@@ -1,47 +1,74 @@
 'use strict';
-/* Day section code */
-
-    var d = new Date();
-    var weekday = new Array(7);
-    weekday[0] = "Domingo";
-    weekday[1] = "Lunes";
-    weekday[2] = "Martes";
-    weekday[3] = "Miércoles";
-    weekday[4] = "Jueves";
-    weekday[5] = "Viernes";
-    weekday[6] = "Sábado";
-
-    var n = weekday[d.getDay()];
-    document.getElementById("weekday").innerHTML = n;
-
-/* Day number code */
-
-var d = new Date();
-document.getElementById("daynumber").innerHTML = d.getDay();
 
 
-/* Month names code */
+var btnMore = document.querySelector('#btnMore');
+btnMore.addEventListener("click", showForm);
 
-var w1 = new Date();
-var month = new Array(11);
-    month[0] = "Enero";
-    month[1] = "Febrero";
-    month[2] = "Marzo";
-    month[3] = "Abril";
-    month[4] = "Mayo";
-    month[5] = "Junio";
-    month[6] = "Julio";
-    month[7] = "Agosto";
-    month[8] = "Septiembre";
-    month[9] = "Octubre";
-    month[10] = "Noviembre";
-    month[11] = "Diciembre";
+var btnAddElement = document.querySelector('#btnAdd');
+btnAddElement.addEventListener("click", saveTask);
 
-   var w2 = month[w1.getMonth()];
-   document.getElementById("month").innerHTML = w2;
+var returnTask = JSON.parse(localStorage.getItem('arrTask_task'));
+
+getAndShowTasks();
+
+function getAndShowTasks(){
+  //comprobamos si hay algun valor en localStorage
+    var detail_task = '';
+    for(var i = 0;i<returnTask.length;i++)
+    {
+      detail_task += '<li id="li"><input class="check" id="check" type="checkbox">'+returnTask[i]+'</li>';
+    }
 
 
-/* Year section code*/
+    var tTask = document.querySelector('#allTask');
+    tTask.innerHTML  = detail_task;
+    textTask.value = '';
 
-var d = new Date();
-document.getElementById("year").innerHTML = d.getFullYear();
+}
+
+/* Funcion que vacia los datos de localStorage */
+function ResetTasks(){
+  returnTask = [];
+  localStorage.setItem('arrTask_task',JSON.stringify(returnTask));
+}
+
+function saveTask(){
+  var taskContainerMenu = document.querySelector('#container__form');
+  var newTask = document.querySelector('#textTask');
+  var textTask = newTask.value;
+
+  if(textTask == 'empty')
+  {
+    ResetTasks();
+  }
+  else
+  {
+    returnTask.push(textTask);
+    localStorage.setItem('arrTask_task',JSON.stringify(returnTask));
+    /* Mostramos el localStorage que tenemos guardado */
+  }
+
+  getAndShowTasks();
+  taskContainerMenu.style.display = 'none';
+  var containerTask = document.querySelector('#allTask');
+  containerTask.classList.remove ('container__task--opacity');
+
+}
+//console.log(saveTask());
+
+ function showForm(){
+   var taskContainerMenu = document.querySelector('#container__form');
+   var containerTask = document.querySelector('#allTask');
+   taskContainerMenu.style.display = 'block';
+   containerTask.className += " container__task--opacity";
+ }
+
+//   var check = document.querySelector('#check');
+// //   if (check.checked==true){
+// //
+// // }
+// var li= document.querySelector('#li')
+// if (document.querySelector('#check').checked)
+//  {
+//    li.className += " li_2";
+//  }
