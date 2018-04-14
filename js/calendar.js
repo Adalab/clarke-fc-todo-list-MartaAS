c084369b3a68d1ce812dc8c91b3cf0e50a2890a2
Marta Aguilar <martaguilar.soto@gmail.com>
2018-04-14 18:11:01 +0200

    clean

--- js/calendar.js
+++ js/calendar.js
@@ -1,39 +1,39 @@
 'use strict';
 
 /* Day section code */
-    var today = new Date();
-    var weekday = new Array(7);
-    weekday[0] = "Domingo";
-    weekday[1] = "Lunes";
-    weekday[2] = "Martes";
-    weekday[3] = "Miércoles";
-    weekday[4] = "Jueves";
-    weekday[5] = "Viernes";
-    weekday[6] = "Sábado";
+var today = new Date();
+var weekday = new Array(7);
+weekday[0] = "Domingo";
+weekday[1] = "Lunes";
+weekday[2] = "Martes";
+weekday[3] = "Miércoles";
+weekday[4] = "Jueves";
+weekday[5] = "Viernes";
+weekday[6] = "Sábado";
 
-    var dayName = weekday[today.getDay()];
-    document.getElementById("weekday").innerHTML = dayName;
+var dayName = weekday[today.getDay()];
+document.getElementById("weekday").innerHTML = dayName;
 
 /* Day number code */
-  document.getElementById("daynumber").innerHTML = today.getDate();
+document.getElementById("daynumber").innerHTML = today.getDate();
 
 /* Month names code */
 var month = new Array(11);
-    month[0] = "Enero";
-    month[1] = "Febrero";
-    month[2] = "Marzo";
-    month[3] = "Abril";
-    month[4] = "Mayo";
-    month[5] = "Junio";
-    month[6] = "Julio";
-    month[7] = "Agosto";
-    month[8] = "Septiembre";
-    month[9] = "Octubre";
-    month[10] = "Noviembre";
-    month[11] = "Diciembre";
+month[0] = "Enero";
+month[1] = "Febrero";
+month[2] = "Marzo";
+month[3] = "Abril";
+month[4] = "Mayo";
+month[5] = "Junio";
+month[6] = "Julio";
+month[7] = "Agosto";
+month[8] = "Septiembre";
+month[9] = "Octubre";
+month[10] = "Noviembre";
+month[11] = "Diciembre";
 
-   var monthName = month[today.getMonth()];
-   document.getElementById("month").innerHTML = monthName;
+var monthName = month[today.getMonth()];
+document.getElementById("month").innerHTML = monthName;
 
 /* Year section code*/
 document.getElementById("year").innerHTML =  ', '+ today.getFullYear();
--- js/main.js
+++ js/main.js
@@ -9,50 +9,45 @@ btnAddElement.addEventListener("click", saveTask);
 
 var returnTask = JSON.parse(localStorage.getItem('arrTask'));
 if(returnTask)
-  getAndShowTasks();
+getAndShowTasks();
 else
-  var returnTask= [];
-
-
-  //console.log(returnTask);
-
-
+var returnTask= [];
 
 function getAndShowTasks(){
   //comprobamos si hay algun valor en localStorage
-    var detail_task = '';
-    for(var i = 0;i<returnTask.length;i++){
-      if(returnTask[i].check == 0){
-        detail_task += '<li class="li"><input class="check" type="checkbox">'+returnTask[i].name +'</li>';
-       }
-      else{
-        detail_task += '<li class="li li__line--througth"><input class="check" type="checkbox" checked>'+returnTask[i].name +'</li>';
-
-      }
+  var detail_task = '';
+  for(var i = 0;i<returnTask.length;i++){
+    if(returnTask[i].check == 0){
+      detail_task += '<li class="li"><input class="check" type="checkbox">'+returnTask[i].name +'</li>';
+    }
+    else{
+      detail_task += '<li class="li li__line--througth"><input class="check" type="checkbox" checked>'+returnTask[i].name +'</li>';
 
     }
 
-    var tTask = document.querySelector('#allTask');
-    tTask.innerHTML  = detail_task;
-    textTask.value = '';
-    //CreateEventsToCheckBoxes();
+  }
+
+  var tTask = document.querySelector('#allTask');
+  tTask.innerHTML  = detail_task;
+  textTask.value = '';
+  //CreateEventsToCheckBoxes();
 }
 
 function CreateEventsToCheckBoxes(){
-var nombres = document.querySelectorAll('.container__task .li');
+  var nombres = document.querySelectorAll('.container__task .li');
   for(var i=0;i<nombres.length;i++)
   {
     var element = nombres[i];
 
     //element.addEventListener('click', LineCross(nombres[i]));
-    }
-        console.log('llego1');
-    document.getElementById("allTask").addEventListener("click",function(e) {
-      console.log('llego');
-  // if (e.target && e.target.matches("li.item")) {
-  //   e.target.className = "foo"; // new class name here
-   //}
-});
+  }
+  console.log('llego1');
+  document.getElementById("allTask").addEventListener("click",function(e) {
+    console.log('llego');
+    // if (e.target && e.target.matches("li.item")) {
+    //   e.target.className = "foo"; // new class name here
+    //}
+  });
 }
 
 
@@ -66,7 +61,7 @@ function LineCross(element){
   }
   else
   {
-     element.classList.remove('li__line--througth');
+    element.classList.remove('li__line--througth');
   }
   updateTaskCheck();
 }
@@ -95,7 +90,7 @@ function saveTask(){
     else
     {
       //unshift para poner en primer lugar del array, shift para poner el ultimo(push)
-//console.log(newAllTask);
+      //console.log(newAllTask);
 
       returnTask.unshift(newAllTask);
       localStorage.setItem('arrTask',JSON.stringify(returnTask));
@@ -108,92 +103,92 @@ function saveTask(){
     var containerTask = document.querySelector('#allTask');
     containerTask.classList.remove ('opacity');
   }
-  }
+}
 
 //console.log(saveTask());
 
- function showForm(){
-   var taskContainerMenu = document.querySelector('#container__form');
-   var containerTask = document.querySelector('#allTask');
-   // taskContainerMenu.style.display = 'block';
-   //containerTask.className += " container__task--opacity";
+function showForm(){
+  var taskContainerMenu = document.querySelector('#container__form');
+  var containerTask = document.querySelector('#allTask');
+  // taskContainerMenu.style.display = 'block';
+  //containerTask.className += " container__task--opacity";
 
-if(taskContainerMenu.style.display == 'flex'){
-  //console.log('1');
-  taskContainerMenu.style.display = 'none';
-  containerTask.classList.remove ('opacity');
+  if(taskContainerMenu.style.display == 'flex'){
+    //console.log('1');
+    taskContainerMenu.style.display = 'none';
+    containerTask.classList.remove ('opacity');
+  }
+  else{
+    //console.log('0');
+    taskContainerMenu.style.display = 'flex';
+    containerTask.classList.add ('opacity');
+  }
 }
-else{
-  //console.log('0');
-  taskContainerMenu.style.display = 'flex';
-  containerTask.classList.add ('opacity');
-}
- }
 
 
 var nombres = document.querySelectorAll('.container__task .li');
 //console.log(nombres);
 var _toggle_tachar = function(element){
-element.addEventListener('click', function(){
+  element.addEventListener('click', function(){
 
-var checked = element.querySelector("input[type='checkbox']:checked");
+    var checked = element.querySelector("input[type='checkbox']:checked");
 
-if(checked)
-{
-  //console.log(element.outerText);
-  element.classList.add('li__line--througth');
+    if(checked)
+    {
+      //console.log(element.outerText);
+      element.classList.add('li__line--througth');
 
-/*
-  var elemento_selecccionado = returnTask.filter(t => t.name.includes(element.outerText));
-elemento_selecccionado[0].check = 1;
-  console.log(elemento_selecccionado[0].check);*/
+      /*
+      var elemento_selecccionado = returnTask.filter(t => t.name.includes(element.outerText));
+      elemento_selecccionado[0].check = 1;
+      console.log(elemento_selecccionado[0].check);*/
+    }
+    else
+    {
+      //console.log(element.outerText);
+      element.classList.remove('li__line--througth');
+
+      /*var elemento_selecccionado = returnTask.filter(t => t.name.includes(element.outerText));
+      elemento_selecccionado[0].check = 0;
+      console.log(elemento_selecccionado[0].check);*/
+    }
+
+    updateTaskCheck();
+  });
 }
-else
-{
-  //console.log(element.outerText);
-   element.classList.remove('li__line--througth');
 
-   /*var elemento_selecccionado = returnTask.filter(t => t.name.includes(element.outerText));
-   elemento_selecccionado[0].check = 0;
-   console.log(elemento_selecccionado[0].check);*/
+
+
+for( var i=0; i<nombres.length; i++ ){
+  _toggle_tachar(nombres[i]);
 }
-
-updateTaskCheck();
-});
-}
-
-
-
- for( var i=0; i<nombres.length; i++ ){
- 	_toggle_tachar(nombres[i]);
-  }
 
 
 //funcion para guardar valor del check y machacar array anterior
 function updateTaskCheck(){
-    nombres = document.querySelectorAll('.container__task .li');
-    returnTask = [];
-    var object = {'name': '', 'check':0};
-    var element = '';
-    var checked = 0;
-    for(var i = 0 ; i < nombres.length; i++ ){
+  nombres = document.querySelectorAll('.container__task .li');
+  returnTask = [];
+  var object = {'name': '', 'check':0};
+  var element = '';
+  var checked = 0;
+  for(var i = 0 ; i < nombres.length; i++ ){
 
-        element = nombres[i];
-        checked = element.querySelector("input[type='checkbox']:checked");
-        if(checked)
-        checked = 1;
-        else
-        checked = 0;
-        object = {'name': element.outerText, 'check': checked}
+    element = nombres[i];
+    checked = element.querySelector("input[type='checkbox']:checked");
+    if(checked)
+    checked = 1;
+    else
+    checked = 0;
+    object = {'name': element.outerText, 'check': checked}
 
 
 
-        returnTask.push(object);
-        //console.log(element.outerText + '-'+checked);
+    returnTask.push(object);
+    //console.log(element.outerText + '-'+checked);
 
-    }
+  }
 
-    //console.log(returnTask);
-    //returnTask.unshift(newAllTask);
-    localStorage.setItem('arrTask',JSON.stringify(returnTask));
+  //console.log(returnTask);
+  //returnTask.unshift(newAllTask);
+  localStorage.setItem('arrTask',JSON.stringify(returnTask));
 }
