35e8ffd3690f0d996a80e6d438a47803e82a0404
Marta Aguilar <martaguilar.soto@gmail.com>
2018-04-14 19:14:45 +0200

    clean

--- js/main.js
+++ js/main.js
@@ -7,12 +7,17 @@ btnMore.addEventListener("click", showForm);
 var btnAddElement = document.querySelector('#btnAdd');
 btnAddElement.addEventListener("click", saveTask);
 
+//array guardado en LS
 var returnTask = JSON.parse(localStorage.getItem('arrTask'));
-if(returnTask)
-getAndShowTasks();
-else
-var returnTask= [];
 
+if(returnTask){
+  getAndShowTasks();
+}
+else{
+  returnTask= [];
+}
+
+//
 function getAndShowTasks(){
   //comprobamos si hay algun valor en localStorage
   var detail_task = '';
@@ -22,48 +27,12 @@ function getAndShowTasks(){
     }
     else{
       detail_task += '<li class="li li__line--througth"><input class="check" type="checkbox" checked>'+returnTask[i].name +'</li>';
-
     }
-
   }
 
   var tTask = document.querySelector('#allTask');
   tTask.innerHTML  = detail_task;
   textTask.value = '';
-  //CreateEventsToCheckBoxes();
-}
-
-function CreateEventsToCheckBoxes(){
-  var nombres = document.querySelectorAll('.container__task .li');
-  for(var i=0;i<nombres.length;i++)
-  {
-    var element = nombres[i];
-
-    //element.addEventListener('click', LineCross(nombres[i]));
-  }
-  console.log('llego1');
-  document.getElementById("allTask").addEventListener("click",function(e) {
-    console.log('llego');
-    // if (e.target && e.target.matches("li.item")) {
-    //   e.target.className = "foo"; // new class name here
-    //}
-  });
-}
-
-
-function LineCross(element){
-  console.log("element"+element);
-  var checked = element.querySelector("input[type='checkbox']:checked");
-  //console.log("checked"+checked);
-  if(checked)
-  {
-    element.classList.add('li__line--througth');
-  }
-  else
-  {
-    element.classList.remove('li__line--througth');
-  }
-  updateTaskCheck();
 }
 
 /* Funcion que vacia los datos de localStorage */
@@ -90,14 +59,10 @@ function saveTask(){
     else
     {
       //unshift para poner en primer lugar del array, shift para poner el ultimo(push)
-      //console.log(newAllTask);
-
       returnTask.unshift(newAllTask);
       localStorage.setItem('arrTask',JSON.stringify(returnTask));
       /* Mostramos el localStorage que tenemos guardado */
     }
-
-
     getAndShowTasks();
     taskContainerMenu.style.display = 'none';
     var containerTask = document.querySelector('#allTask');
@@ -105,21 +70,19 @@ function saveTask(){
   }
 }
 
-//console.log(saveTask());
 
 function showForm(){
   var taskContainerMenu = document.querySelector('#container__form');
   var containerTask = document.querySelector('#allTask');
-  // taskContainerMenu.style.display = 'block';
-  //containerTask.className += " container__task--opacity";
+
 
   if(taskContainerMenu.style.display == 'flex'){
-    //console.log('1');
+
     taskContainerMenu.style.display = 'none';
     containerTask.classList.remove ('opacity');
   }
   else{
-    //console.log('0');
+
     taskContainerMenu.style.display = 'flex';
     containerTask.classList.add ('opacity');
   }
@@ -133,36 +96,19 @@ var _toggle_tachar = function(element){
 
     var checked = element.querySelector("input[type='checkbox']:checked");
 
-    if(checked)
-    {
-      //console.log(element.outerText);
+    if(checked){
       element.classList.add('li__line--througth');
-
-      /*
-      var elemento_selecccionado = returnTask.filter(t => t.name.includes(element.outerText));
-      elemento_selecccionado[0].check = 1;
-      console.log(elemento_selecccionado[0].check);*/
     }
-    else
-    {
-      //console.log(element.outerText);
+    else{
       element.classList.remove('li__line--througth');
-
-      /*var elemento_selecccionado = returnTask.filter(t => t.name.includes(element.outerText));
-      elemento_selecccionado[0].check = 0;
-      console.log(elemento_selecccionado[0].check);*/
     }
-
     updateTaskCheck();
   });
 }
 
-
-
 for( var i=0; i<nombres.length; i++ ){
   _toggle_tachar(nombres[i]);
 }
-
 
 //funcion para guardar valor del check y machacar array anterior
 function updateTaskCheck(){
@@ -180,15 +126,7 @@ function updateTaskCheck(){
     else
     checked = 0;
     object = {'name': element.outerText, 'check': checked}
-
-
-
     returnTask.push(object);
-    //console.log(element.outerText + '-'+checked);
-
   }
-
-  //console.log(returnTask);
-  //returnTask.unshift(newAllTask);
   localStorage.setItem('arrTask',JSON.stringify(returnTask));
 }
